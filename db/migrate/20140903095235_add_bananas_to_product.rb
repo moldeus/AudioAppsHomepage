13d6c3acca0dcdd8ae60f02196cad0f4bb8a49f5
moldeus <klas.moldeus@gmail.com>
2014-09-04 16:38:52 +0200

    new repository

--- db/migrate/20140903095235_add_bananas_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddBananasToProduct < ActiveRecord::Migration
-  def change
-    add_column :products, :bananas, :string
-  end
-end
--- db/migrate/20140903095321_add_premium_customer_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPremiumCustomerToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :premium_customer, :boolean
-  end
-end
