bd3cfad3e5e7c725135e2aa12fa8b11c91d29dc8
moldeus <klas.moldeus@gmail.com>
2014-09-05 15:20:31 +0200

    Added category to Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903100537) do
+ActiveRecord::Schema.define(version: 20140905130618) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140903100537) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "prices"
+    t.integer  "category"
   end
 
 end
